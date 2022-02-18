bff9a8372de89fca0151e29e0194e5a487d518e0
shotaro0522 <68640527+shotaro0522@users.noreply.github.com>
2022-02-18 20:44:28 +0900

    一覧表示と編集機能の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,9 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_recipe, only: [:edit, :update]
 
   def index
+    @recipes = Recipe.order(created_at: "DESC")
   end
 
   def new
@@ -18,9 +20,29 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    recipe_attributes = @recipe.attributes
+    @recipe_form = RecipeForm.new(recipe_attributes)
+  end
+
+  def update
+    @recipe_form = RecipeForm.new(recipe_form_params)
+    @recipe_form.image ||= @recipe.image.blob
+    if @recipe_form.valid?
+      @recipe_form.update(recipe_form_params, @recipe)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
   def recipe_form_params
     params.require(:recipe_form).permit(:recipe_name, :genre_id, :info, :url, :image).merge(user_id: current_user.id)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 
   
--- app/models/recipe_form.rb
+++ app/models/recipe_form.rb
@@ -1,7 +1,10 @@
 class RecipeForm
   include ActiveModel::Model
 
-  attr_accessor :user_id, :recipe_name, :genre_id, :info, :url, :image
+  attr_accessor(
+    :user_id, :recipe_name, :genre_id, :info, :url, :image,
+    :id, :created_at, :datetime, :updated_at, :datetime
+  )
   
   with_options presence: true do
     validates :user_id
@@ -13,6 +16,10 @@ class RecipeForm
 
   def save
     Recipe.create(user_id: user_id, recipe_name: recipe_name, genre_id: genre_id, info: info, url: url, image: image)
+  end
+
+  def update(params, recipe)
+    recipe.update(params)
   end
 
 end
